72762771e9e993ce1a3c032227ba79d20599c15b
Pradyumna Gayake <prgayake100@gmail.com>
2025-02-18 16:33:04 +0530

    Initial commit

---
+++ src/test/java/com/mongo/db/demo/MongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
